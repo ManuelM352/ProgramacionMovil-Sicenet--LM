ee87f6d6ad7958a0641fb6e3ec05a475c4e36516
Manuel <perezmane07@gmail.com>
2024-02-17 03:08:08 -0600

    Avance: Integración de solicitud al servidor

---
+++ app/src/main/java/com/example/appsicenet/data/RetrofitClient.kt
@@ -0,0 +1,52 @@
+package com.example.appsicenet.data
+
+import android.content.Context
+import android.util.Log
+import com.example.appsicenet.network.AddCookiesInterceptor
+import com.example.appsicenet.network.ReceivedCookiesInterceptor
+import com.example.appsicenet.network.SICENETApiService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+
+
+class RetrofitClient(context: Context) {
+        private val BASE_URL = "https://sicenet.surguanajuato.tecnm.mx"
+
+        private val client = OkHttpClient.Builder()
+            .addInterceptor(AddCookiesInterceptor(context))
+            .addInterceptor(ReceivedCookiesInterceptor(context))
+            .addInterceptor(createLoggingInterceptor())
+            .build()
+
+        private fun createLoggingInterceptor(): Interceptor {
+            return Interceptor { chain ->
+                val request = chain.request()
+                val requestHeaders = request.headers
+                for (i in 0 until requestHeaders.size) {
+                    Log.d("HEADER", "${requestHeaders.name(i)}: ${requestHeaders.value(i)}")
+                }
+                Log.d("Solicitud", "URL: ${request.url}")
+                Log.d("Solicitud", "Método: ${request.method}")
+                Log.d("Solicitud", "Cuerpo: ${request.body}")
+                val response = chain.proceed(request)
+                Log.d("Respuesta", "Código: ${response.code}")
+                Log.d("Respuesta", "Cuerpo: ${response.body?.string()}")
+                response
+            }
+        }
+
+        private val retrofit: Retrofit = Retrofit.Builder()
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .client(client)
+            .baseUrl(BASE_URL)
+            .build()
+
+        val retrofitService: SICENETApiService by lazy {
+            retrofit.create(SICENETApiService::class.java)
+        }
+
+}
--- app/src/main/java/com/example/appsicenet/screens/LoginScreen.kt
+++ app/src/main/java/com/example/appsicenet/screens/LoginScreen.kt
@@ -1,5 +1,8 @@
 package com.example.appsicenet.screens
 
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -17,12 +20,26 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.example.appsicenet.data.RetrofitClient
+import com.example.appsicenet.models.AccessLoginResponse
+import com.example.appsicenet.models.LoginResult
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 @Composable
 fun LoginScreen() {
     var matricula by remember { mutableStateOf("") }
     var contrasenia by remember { mutableStateOf("") }
+    val context = LocalContext.current
 
     Column(
         modifier = Modifier
@@ -46,7 +63,10 @@ fun LoginScreen() {
         )
         Spacer(modifier = Modifier.height(16.dp))
         Button(
-            onClick = {  },
+
+            onClick = {
+
+                authenticate(context,matricula, contrasenia) },
             modifier = Modifier.fillMaxWidth()
         ) {
             Text("Iniciar sesión")
@@ -54,3 +74,77 @@ fun LoginScreen() {
     }
 }
 
+
+private fun authenticate(context: Context, matricula: String, contrasenia: String) {
+    val bodyLogin = loginRequestBody(matricula, contrasenia)
+    val service = RetrofitClient(context).retrofitService
+
+    service.login(bodyLogin).enqueue(object : Callback<ResponseBody> {
+        override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>){
+            if (response.isSuccessful) {
+
+                // Obtener la cadena de respuesta completa
+                val responseBodyString = response.body()?.toString()
+
+                // Imprimir la cadena de respuesta en el log
+                Log.w("Respuesta completa", responseBodyString ?: "Cuerpo de respuesta vacío")
+
+                // Deserializar la respuesta JSON
+                val json = Json { ignoreUnknownKeys = true } // Configuración opcional según tus necesidades
+
+                try {
+                    val accessLoginResponse = json.decodeFromString<AccessLoginResponse>(responseBodyString ?: "")
+                    // Aquí puedes manejar el objeto deserializado según tus necesidades
+                    Log.w("Objeto deserializado", accessLoginResponse.toString())
+
+                    val accessResultJson: String? = accessLoginResponse.body?.accesoLoginResponse?.accesoLoginResult
+                    if (!accessResultJson.isNullOrBlank()) {
+                        val accessResult: LoginResult = json.decodeFromString(accessResultJson)
+
+                    } else {
+                        showError(context, "Error: La cadena JSON de accesoLoginResult está vacía o nula")
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    showError(context, "Error en la deserialización de la respuesta")
+                }
+
+            } else {
+                showError(context, "Error en la autenticación. Código de respuesta: ${response.code()}")
+            }
+        }
+
+        override fun onFailure(call: Call<ResponseBody>, t: Throwable){
+            t.printStackTrace()
+            showError(context, "Error en la solicitud")
+        }
+    })
+}
+
+private fun loginRequestBody(matricula: String, contrasenia: String): RequestBody {
+    return """
+        <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+          <soap:Body>
+            <accesoLogin xmlns="http://tempuri.org/">
+              <strMatricula>$matricula</strMatricula>
+              <strContrasenia>$contrasenia</strContrasenia>
+              <tipoUsuario>ALUMNO</tipoUsuario>
+            </accesoLogin>
+          </soap:Body>
+        </soap:Envelope>
+    """.trimIndent().toRequestBody("text/xml; charset=utf-8".toMediaTypeOrNull())
+}
+
+public fun profileRequestBody(): RequestBody {
+    return """
+        <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+          <soap:Body>
+            <getAlumnoAcademicoWithLineamiento xmlns="http://tempuri.org/" />
+          </soap:Body>
+        </soap:Envelope>
+    """.trimIndent().toRequestBody("text/xml; charset=utf-8".toMediaTypeOrNull())
+}
+
+private fun showError(context: Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+}
