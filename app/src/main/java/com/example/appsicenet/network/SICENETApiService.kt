db5a95ae8b4d13f71549bc7d9830587366c86669
Manuel <perezmane07@gmail.com>
2024-02-17 02:10:56 -0600

    Avance: Integracion del archivo ApiServices para la obtención de datos por medio de POST, asi mismo se integra el permiso de internet y los gradles necesarios para la aplicación.

---
+++ app/src/main/java/com/example/appsicenet/network/SICENETApiService.kt
@@ -0,0 +1,25 @@
+package com.example.appsicenet.network
+
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface SICENETApiService {
+    @Headers(
+        "Content-Type: text/xml; charset=utf-8",
+        "SOAPAction: http://tempuri.org/accesoLogin"
+    )
+    @POST("/ws/wsalumnos.asmx")
+    fun login(@Body body: RequestBody): Call<ResponseBody>
+
+    @Headers(
+        "Content-Type: text/xml; charset=utf-8",
+        "SOAPAction: http://tempuri.org/getAlumnoAcademicoWithLineamiento"
+    )
+    @POST("/ws/wsalumnos.asmx")
+    fun getAcademicProfile(@Body body: RequestBody): Call<ResponseBody>
+
+}
